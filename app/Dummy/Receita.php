719fe232eab82d5f4c035cda90093170ae8530f5
devlulcas <lucasalvesrego.contato@gmail.com>
2022-02-23 00:51:11 -0300

    add: dados para validacao do xml de receita

---
+++ app/Dummy/Receita.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Dummy;
+
+class Receita
+{
+  public static $falsaReceitaEmArray = [
+    "receita" => [
+      "titulo" => "Pão simples",
+      "ingredientes" => [
+        "ingrediente" => [
+          "Farinha de Trigo",
+          "Fermento",
+          "Água",
+          "Sal",
+        ]
+      ],
+      "instrucoes" => [
+        "passo" => [
+          "Misture todos os ingredientes, e dissolva bem.",
+          "Cubra com um pano e deixe por uma hora em um local morno.",
+          "Misture novamente, coloque numa bandeja e asse num forno.",
+        ]
+      ],
+    ],
+  ];
+}
